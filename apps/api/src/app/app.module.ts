13d065d5e672f049e9df9be3260f64e8371c6110
Martial Anouman <martialanouman@arolitec.com>
2022-08-23 15:02:38 +0000

    setup graphql module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { environment } from '../environments/environment'
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: !environment.production,
+      playground: !environment.production,
+      typePaths: ['./**/*.graphql'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
