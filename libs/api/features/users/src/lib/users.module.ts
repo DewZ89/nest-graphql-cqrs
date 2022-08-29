52ff0669370504a691ed00f54b38f434033a2fd2
Martial Anouman <martialanouman@arolitec.com>
2022-08-29 12:11:29 +0000

    feat(api-shared-prisma): set prisma module global to avoid repetitive import
    Signed-off-by: Martial Anouman <martialanouman@arolitec.com>

--- libs/api/features/users/src/lib/users.module.ts
+++ libs/api/features/users/src/lib/users.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common'
 import { PasswordService, UsersService } from './services'
 import { UsersResolver } from './users.resolver'
-import { PrismaModule } from '@blog/api/shared/prisma'
 import { HANDLERS as QUERY_HANDLERS } from './queries'
 import { HANDLERS as COMMAND_HANDLERS } from './commands'
 import { CqrsModule } from '@nestjs/cqrs'
 
 @Module({
-  imports: [PrismaModule, CqrsModule],
+  imports: [CqrsModule],
   controllers: [],
   providers: [
     UsersService,
